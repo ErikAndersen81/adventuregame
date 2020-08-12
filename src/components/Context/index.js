66f41570f5ef5d3194383a775c81da928c9f27ce
Erik Andersen <erand17@student.sdu.dk>
2020-08-12 11:02:47 +0200

    Removed unused contexts

--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -1,6 +1,4 @@
-import GraphicsContext from './GraphicsContext.js';
-import PlayerCanvasContext from './PlayerCanvasContext.js';
 import PlayerContext from './PlayerContext.js';
 import CanvasContext from './CanvasContext.js';
 
-export { GraphicsContext, PlayerCanvasContext, PlayerContext, CanvasContext };
+export { PlayerContext, CanvasContext };
