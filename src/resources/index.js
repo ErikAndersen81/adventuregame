3b3dd2cbc4089a91b11c4d0f98ab397a4e400bab
Erik Andersen <erand17@student.sdu.dk>
2020-08-14 13:14:39 +0200

    removed js.map files

--- src/resources/index.js
+++
@@ -1,8 +0,0 @@
-import images from './images';
-const floors = images.filter(img => img.group === 'floor');
-const walls = images.filter(img => img.group === 'wall');
-const keys = images.filter(img => img.group === 'key');
-const doors = images.filter(img => img.group === 'door');
-const players = images.filter(img => img.group === 'player');
-export { floors, walls, keys, doors, players };
-//# sourceMappingURL=index.js.map
